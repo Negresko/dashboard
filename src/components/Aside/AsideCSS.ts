a6bdbe4721cbca04596fea6f70cf5e7d7b528b1c
Negresko <viniciuspeixoto2008@hotmail.com>
2023-01-11 22:11:59 -0300

    Estilos modificados!

--- src/components/Aside/AsideCSS.ts
+++ src/components/Aside/AsideCSS.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const ContainerAside = styled.div`
-grid-area: AS;
-color: ${props => props.theme.colors.white};
-background-color: ${props => props.theme.colors.tertiary};
+    grid-area: AS;
+    color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.tertiary};
 
 `;
--- src/components/MainHeader/MainHeaderCSS.ts
+++ src/components/MainHeader/MainHeaderCSS.ts
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 export const ContainerMainHeader = styled.div`
-grid-area: MH;
-color: ${props => props.theme.colors.white};
-background-color: ${props => props.theme.colors.secondary};
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 0 20px;
-border-bottom: 1px solid ${props => props.theme.colors.gray};
+    grid-area: MH;
+    color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.secondary};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    border-bottom: 1px solid ${props => props.theme.colors.gray};
 `;
 
 export const Profile = styled.div`
-color: ${props => props.theme.colors.white};
+    color: ${props => props.theme.colors.white};
 `;
 
 export const Welcome = styled.h3`
