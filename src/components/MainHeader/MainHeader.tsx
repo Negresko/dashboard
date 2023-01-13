08a3866ce8fe01f6dc0a1467db587f5df3c3e33b
Negresko <viniciuspeixoto2008@hotmail.com>
2023-01-13 17:53:19 -0300

    Modificando MainHeader

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -9,7 +9,7 @@ const MainHeader: React.FC = () => {
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length);
     return emojis[indice];
-  }, [])
+  }, []);
 
   return (
     <ContainerMainHeader>
